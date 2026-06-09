c112b07ecd745f3cd4888759bfb6d6a168c2deb6
jonathan da silva ferreira <jonathanferreiratr@gmail.com>
2026-06-09 00:58:18 -0300

    criando limpeza e ler texto

---
+++ edemc.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+void limpar_entrada() {
+    char c;
+    while ((c = getchar()) != '\n' && c != EOF) {}
+}
+
+void ler_texto(char *buffer, int length){
+    fgets(buffer, length, stdin);
+    strtok(buffer, "\n");
+}
+
